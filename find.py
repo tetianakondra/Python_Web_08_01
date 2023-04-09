e7e1aa9868080046d1728f2488e7574eda512e55
tetianakondra <tatjana.ukon@gmail.com>
2023-04-09 11:45:15 +0300

    DB with cache-option

---
+++ find.py
@@ -0,0 +1,82 @@
+import json
+
+from mongoengine import disconnect
+import redis
+from redis_lru import RedisLRU
+
+from models import Author, Quote
+
+client = redis.StrictRedis(host = "localhost", port = 6379, password=None)
+cache = RedisLRU(client)
+
+@cache
+def find_author(name):
+    print("the first call")
+    authors = Author.objects(fullname__contains=(name))
+    for auth in authors:
+        quotes = Quote.objects(author=auth.id)
+        for quote in quotes:
+            return f"Author with symbols in name {name} said: {quote.quote}"
+        
+
+@cache
+def find_tag(searching_tag):
+    print("the first call")
+    quotes = Quote.objects()
+    quote_ids = set()
+    quote_list = []
+    for quo in quotes:
+        for tag in quo.tags:
+            if searching_tag in tag:
+                quote_ids.add(quo.id)
+    for el in quote_ids:
+        quote = Quote.objects(id=el)
+        quote_list.append(quote[0].quote)
+    return quote_list
+
+
+@cache
+def find_tags(searching_tags):
+    print("the first call")
+    tags = (searching_tags).split(",")
+    quotes = Quote.objects()
+    quote_ids = set()  
+    quote_list = [] 
+    for quo in quotes:
+        for tag in tags:
+            if tag in quo.tags:
+                quote_ids.add(quo.id)
+    for el in quote_ids:
+        quote = Quote.objects(id=el)
+        quote_list.append(quote[0].quote)
+    return quote_list
+
+
+def  main():
+        
+    while True:
+        user_command = input("Enter <command>: <data for serching> or 'exit' to end the programm ")
+        user_search = user_command.find(": ")
+        
+        if user_command.lower() == "exit":
+            print("Goodbye")
+            disconnect()
+            break
+
+        if user_command[0:user_search] == "name":
+            print(find_author(user_command[user_search+2:]))
+
+
+        if user_command[0:user_search] == "tag":
+            print(find_tag(user_command[user_search+2:]))
+
+
+        if user_command[0:user_search] == "tags":
+            print(find_tags(user_command[user_search+2:]))
+
+
+    disconnect()
+    
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+
+from mongoengine import *
+from mongoengine import EmbeddedDocument, Document
+from mongoengine import connect
+from mongoengine.fields import BooleanField, DateTimeField, EmbeddedDocumentField, ListField, StringField
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read('config.ini')
+
+mongo_user = config.get('DB', 'USER')
+mongodb_pass = config.get('DB', 'PASSWORD')
+db_name = config.get('DB', 'DB_NAME')
+domain = config.get('DB', 'DOMAIN')
+
+
+connect(host=f"""mongodb+srv://{mongo_user}:{mongodb_pass}@{domain}/{db_name}?retryWrites=true&w=majority""", ssl=True)
+
+
+class Author(Document):
+    fullname = StringField(required=True)
+    born_date = DateTimeField()
+    born_location = StringField(max_length=50)
+    description = StringField()
+
+
+class Quote(Document):
+    tags = ListField(StringField(max_length=200))
+    author = ReferenceField(Author, reverse_delete_rule=CASCADE)
+    quote = StringField()
+    meta = {'allow_inheritance': True}
